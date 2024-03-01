9511c26fe07718f35ee3565fab6c6281cbbec2b5
Eleanor Bucke <107644138+eleanorbucke21@users.noreply.github.com>
2024-03-01 13:16:28 +0000

    Declare Array projectGroup

--- src/camarch/EmployeeTest.java
+++ src/camarch/EmployeeTest.java
@@ -16,7 +16,20 @@ public class EmployeeTest {
         Employee employee1 = new Employee("Joe Bloggs", "jb@gmail.com");
         Employee employee2 = new Employee("Ann Banana", "ab@gmail.com");
         Employee employee3 = new Employee("Tom Thumb", "tt@gmail.com");
+        
+        // Declare an array to store Employee
+        Employee[] projectGroup = new Employee[3];
 
+        // Load the array with Employees
+        projectGroup[0] = employee1;
+        projectGroup[1] = employee2;
+        projectGroup[2] = employee3;
+
+        // Display details for each employee
+        for (Employee employee : projectGroup) {
+            displayEmployeeDetails(employee);
+        }
+        
         // Details for each employee
         displayEmployeeDetails(employee1);
         displayEmployeeDetails(employee2);
